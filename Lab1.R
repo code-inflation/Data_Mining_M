b90813062d94271d22d09b533f47f9d70ab88b44
Robin B <code-inflation@users.noreply.github.com>
2018-02-07 12:46:24 +0100

    added some comments for kNN

--- Lab1.R
+++ Lab1.R
@@ -45,33 +45,36 @@ normalize <- function(x) { return ((x - min(x)) / (max(x) - min(x))) }
 iris_normalized <- as.data.frame(lapply(irisData[1:4], normalize))
 summary(iris_normalized$sepal_length) # check if normalized
 
+# creating training data from normalized dataframe
 iris_train <- rbind(iris_normalized[1:25,], iris_normalized[51:75,],  iris_normalized[101:125,])
-iris_train_labels <- c(irisData[1:25,5], irisData[51:75,5],  irisData[101:125,5])
+iris_train_labels <- c(irisData[1:25,5], irisData[51:75,5],  irisData[101:125,5]) # create a vector of training labels
 
 iris_test <- rbind(iris_normalized[26:50,], iris_normalized[76:100,],  iris_normalized[126:150,])
-iris_test_labels <- c(irisData[26:50,5], irisData[76:100,5],  irisData[126:150,5])
+iris_test_labels <- c(irisData[26:50,5], irisData[76:100,5],  irisData[126:150,5]) # create a vector of test labels
 
+# creating predictions with k=sqrt(total values)
 iris_test_pred <- knn(train = iris_train, test = iris_test,cl = iris_train_labels, k=sqrt(nrow(irisData)))
-CrossTable(x = iris_test_labels, y = iris_test_pred, prop.chisq = FALSE)
+CrossTable(x = iris_test_labels, y = iris_test_pred, prop.chisq = FALSE) # print cross table to evaluate predictions
 
-accuracy = function(actual, predicted) { mean(actual == predicted) }
-accuracy(actual = iris_test_labels, iris_test_pred)
+accuracy = function(actual, predicted) { mean(actual == predicted) } # define function to calculate accuracy
+accuracy(actual = iris_test_labels, iris_test_pred) # calculate accuracy for the prediction above
 
+# verify k=sqrt(total values) as best value for k
 k_max <- nrow(iris_train)/3
-
 results <- vector("list", k_max)
 
+# make knn classifier for each k and write the according accuracy into the results vector
 for (k in 1:k_max){
   iris_test_pred <- knn(train = iris_train, test = iris_test,cl = iris_train_labels, k=k)
   a <- accuracy(actual = iris_test_labels, iris_test_pred)
   results[k] <- a
 }
 
+# create a dataframe from the vector
 df <- data.frame(accuracy=matrix(unlist(results), nrow=k_max, byrow=T))
-df$k <- as.numeric(row.names(df))
+df$k <- as.numeric(row.names(df)) # write index to dataframe as it represents k in the vector
 
+# plot a line graph of all k values and the corresponding accuracy
 ggplot(data=df, aes(x=k, y=accuracy, group=1)) +
   geom_line(color = "red")+
   geom_point()
-
-
